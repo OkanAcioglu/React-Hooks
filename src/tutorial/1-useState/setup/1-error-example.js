d4d89de41e1247768758ed82097ee8775c4d2c98
Okan <okanacioglu@gmail.com>
2022-12-05 01:43:02 +0300

    useState-basics

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from 'react'
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
-};
+  let title = 'random title'
+  const handleClick = () => {
+    //! Below changing title like this will not work, title will changed to string as "hello world" as we see in console but component did not re-rendered...
+    title = 'hello people'
+    console.log(title)
+    //! We want two things:
+    //! 1) Keep the value between the renders
+    //! 2) Trigger the render
+  }
+  return (
+    <React.Fragment>
+      <h2>{title}</h2>
+      <button type='button' className='btn' onClick={handleClick}>
+        Change Title
+      </button>
+    </React.Fragment>
+  )
+}
 
-export default ErrorExample;
+export default ErrorExample
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,31 @@
-import React, { useState } from 'react';
-
+import React, { useState } from 'react'
+//! useState is function
+//! It returns an array --> [value, setValue]
+//! initial value can be whatever JS value we want (object,boolean,array,number...)
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
-};
+  // console.log(useState('hello world'))
+  // const value = useState(1)[0]
+  // const handler = useState(1)[1]
+  // console.log(value, handler)
+  const [text, setText] = useState('random title')
 
-export default UseStateBasics;
+  const clickHandler = () => {
+    // if (text === 'random title') {
+    //   setText('Hello World')
+    // } else {
+    //   setText('random title')
+    // }
+    text === 'random title' ? setText('Hello World') : setText('random title')
+  }
+
+  return (
+    <>
+      <h1>{text}</h1>
+      <button className='btn' onClick={clickHandler}>
+        Change Title
+      </button>
+    </>
+  )
+}
+
+export default UseStateBasics
