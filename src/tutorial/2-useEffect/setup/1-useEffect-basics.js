a0c4512b369e8d4810644c16a810d09021cc696a
Okan <okanacioglu@gmail.com>
2022-12-06 00:18:59 +0300

    useEffect-basics

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,25 @@
-import React, { useState, useEffect } from 'react';
-// by default runs after every re-render
+import React, { useState, useEffect } from 'react'
+//! useEffect allows to do side effects
+//! side effect means any work outside of the component that could be changing document title, signing up for subscription, fetching data, setting up event listener... even the console.log considered as side effects...
+//! By default useEffect runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
-};
+  const [value, setValue] = useState(0)
+  //! Took callback function, whatever functionality will place will run after every render...
+  useEffect(() => {
+    console.log('call useEffect')
+    document.title = `New Messages(${value})`
+  })
+  console.log('render component')
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className='btn' onClick={() => setValue(value + 1)}>
+        Click Me
+      </button>
+    </>
+  )
+}
 
-export default UseEffectBasics;
+export default UseEffectBasics
