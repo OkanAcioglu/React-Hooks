fa9e8c118a91f0e171dc9c35bda965194f6aff94
Okan Acıoğlu <okanacioglu@gmail.com>
2022-12-05 13:58:12 +0300

    useState-object

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+//! Array is more straightforward in one case just wipe out the data and set it on the empty array and second thing we had the function that was just returning a new array
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  const [person, setPerson] = useState({
+    //! Pass in object directly
+    name: 'Okan',
+    age: 24,
+    message: 'random message',
+  })
 
-export default UseStateObject;
+  const changeMessageHandler = () => {
+    //! If we do below we wipe out all the data and set the person to string
+    //setPerson('Hello World')
+    //! If we do below we changed the message but we wipe out all the  other data
+    //setPerson({ message: 'Hello World' })
+    //! If we do below we leave the other pairs same and override the message only
+    //! Do not forget to use spread operator when tackling with object in useState
+    setPerson({ ...person, message: 'Hello World' })
+  }
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className='btn' onClick={changeMessageHandler}>
+        Change Message
+      </button>
+    </>
+  )
+}
+
+export default UseStateObject
