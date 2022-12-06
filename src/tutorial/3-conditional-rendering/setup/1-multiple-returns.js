96e8eba19a7564a33f54ff0b21030d338b24da96
Okan Acıoğlu <okanacioglu@gmail.com>
2022-12-06 12:42:23 +0300

    conditional-rendering

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,17 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from 'react'
+const url = 'https://api.github.com/users/QuincyLarson'
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
-};
+  //! Below we have typical loading state or it can be check for user exist or something that kind. And condition mets then we can display one part of the app (one giant component with other multiple component) and if the condition is not met we can display some sign-in page or loading page...
+  const [loading, setLoading] = useState(true)
+  if (loading) {
+    return <h2>Loading...</h2>
+  }
 
-export default MultipleReturns;
+  return <h2>multiple returns</h2>
+}
+
+export default MultipleReturns
+
+//! From JS we know ones we have return, everythink else is just ignored
+//! Conditional Rendering means we will display different components or different content based on some condition.
+//! When we look at the component, we know that it is a function and what can we return? We can return nothing (error occur) or we can have multiple returns
