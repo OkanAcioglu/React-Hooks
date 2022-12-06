4e4dd9c928c1c336438986aefa824f5442765848
Okan Acıoğlu <okanacioglu@gmail.com>
2022-12-06 15:54:22 +0300

    short-circuiting

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
+//! In React when we talk about JSX we talk about the fact that it has to return a expression meaning has to return value. For example if we use if..else.. statement inside the JSX it will not work.
+//! We have a multiple returns but if we have one return and based on some condition, there is going to be either some data shown or hidden staff along lines we would use below operations
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState('')
+  const firstValue = text || 'hello world' // "" is falsey here so "hello world" will be taken
+  const secondValue = text && 'hello world' // "" is falsey so here text will be taken
 
-  return <h2>short circuit</h2>;
-};
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>value : {secondValue}</h1> */}
+      {/* //! Below case working like if text is empty ("") , "Okan" will return as default... We were returning element regardless */}
+      <h1>{text || 'Okan'}</h1>
+      {/* //! Below case working like if text is true , "Hello World" will return as default... We can trigger showing or hiding element */}
+      {!text && <h1>Hello World</h1>}
+    </>
+  )
+}
 
-export default ShortCircuit;
+export default ShortCircuit
