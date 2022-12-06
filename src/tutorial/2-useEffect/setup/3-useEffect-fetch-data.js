86c63bc97cd5e2935f2d2965e65a5a345de91b11
Okan Acıoğlu <okanacioglu@gmail.com>
2022-12-06 11:16:21 +0300

    useEffect-fetch-data

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-const url = 'https://api.github.com/users';
+const url = 'https://api.github.com/users'
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
-};
+  //! Inside the callback we can simply perform as fetch request. We can do this either directly typing inside or we can setup a separate function.
+  //! We will go with separate function with using async-await. We can setup async-await inside the callback as well.
+  //! There is one more rule that we cannot make callback function of useEffect async function because it is simply returns promise. (useEffect(async () => {...}, [])--> We cannot do that)
+  const [users, setUsers] = useState([])
 
-export default UseEffectFetchData;
+  const getUsers = async () => {
+    const response = await fetch(url)
+    const users = await response.json()
+    // setUsers(users)
+    //! If we comment out the above code -> We getting users then we set it into the setUsers so we update the users so this triggers re-render and when we re-render, useEffect triggered and we getting users than set it into setUsers so essentially we are setting up infinite loop.
+    //! Solution is adding dependencies array ([])
+    setUsers(users)
+    console.log(users)
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+  return (
+    <>
+      <h3>github users</h3>
+      <ul className='users'>
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>Profile</a>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
+
+export default UseEffectFetchData
