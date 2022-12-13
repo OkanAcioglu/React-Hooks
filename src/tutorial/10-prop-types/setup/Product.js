eeaf2789dc346a31411e54721dbe50759a066fd8
Okan <okanacioglu@gmail.com>
2022-12-14 01:21:38 +0300

    PropTypes-default-values

--- src/tutorial/10-prop-types/setup/Product.js
+++ src/tutorial/10-prop-types/setup/Product.js
@@ -5,11 +5,14 @@ import PropTypes from 'prop-types'
 import defaultImage from '../../../assets/default-image.jpeg'
 
 const Product = ({ image, name, price }) => {
+  //! Second way assign a default value is using OR operator
+  //! But problem is again in img.url because it will always coming undefined.
+
   return (
     <article className='product'>
-      <img src={image.url} alt={name} />
+      <img src={image?.url || defaultImage} alt={name || 'default name'} />
       <h4>{name}</h4>
-      <p>${price}</p>
+      <p>${price || 3.99}</p>
     </article>
   )
 }
@@ -21,13 +24,13 @@ Product.propTypes = {
   price: PropTypes.number.isRequired,
 }
 //! we can setup default props because simply we do not wanna get error
-Product.defaultProps = {
-  name: 'default name',
-  price: 3.99,
-  //! We import an image from assets folder and if the image is missing by default we will setup this imported image...
-  //! But now problem is in JSX above we used image.url but below there is no URL so we need to fix it with using short circuit operator.
-  image: defaultImage,
-}
+// Product.defaultProps = {
+//   name: 'default name',
+//   price: 3.99,
+//   //! We import an image from assets folder and if the image is missing by default we will setup this imported image...
+//   //! But now problem is in JSX above we used image.url but below there is no URL so we need to fix it with using short circuit operator.
+//   image: defaultImage,
+// }
 
 export default Product
 
