f21e378ddb8c93520a7f43563ee85c9b824add7e
Okan <okanacioglu@gmail.com>
2022-12-13 21:28:41 +0300

    useFetch-custom-hook

--- src/tutorial/9-custom-hooks/setup/1-fetch-example.js
+++ src/tutorial/9-custom-hooks/setup/1-fetch-example.js
@@ -1,24 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import { useFetch } from './2-useFetch'
 
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/javascript-store-products'
 
 const Example = () => {
-  const [loading, setLoading] = useState(true)
-  const [products, setProducts] = useState([])
-
-  const getProducts = async () => {
-    const response = await fetch(url)
-    const products = await response.json()
-    setProducts(products)
-    setLoading(false)
-  }
-
-  useEffect(() => {
-    getProducts()
-  }, [url])
+  //! Here we need to destructure object because we return a object from useFetch custom hook.
+  const { loading, products } = useFetch(url)
+  //! Here we have same result but functionality yucked away in a custom hook...
   console.log(products)
   return (
     <div>
--- src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,3 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
-export const useFetch = () => {};
+export const useFetch = (url) => {
+  const [loading, setLoading] = useState(true)
+  const [products, setProducts] = useState([])
+
+  const getProducts = async () => {
+    const response = await fetch(url)
+    const products = await response.json()
+    setProducts(products)
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [url])
+  return { loading, products }
+}
+
+//! Custom Hooks allow us to reuse the the functionality (fetching data , saving local storage...)
+//! This is a only one example of custom hooks but there are many custom hooks already preapered by other and also we can built our own custom hooks...
+//! Our purpouse here is reuse the fetch functionality so that we do not need to duplicate the codes again and again when we need to fetch data...
+//! Note that especially for this example if we want to use this hook we need to add error cases...
+
+//!!! When we creating custom hook we need to "use" in front of the naming... Either function needs to be a component or it needs to be a custom hook.
