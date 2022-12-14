b332249fb69f985f734d3257eac530ce65eb6474
Okan Acıoğlu <okanacioglu@gmail.com>
2022-12-14 09:38:58 +0300

    React.memo

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect, useCallback, useMemo } from 'react'
-import { useFetch } from '../../9-custom-hooks/final/2-useFetch'
+import { useFetch } from '../../9-custom-hooks/setup/2-useFetch'
 
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/javascript-store-products'
 
 // every time props or state changes, component re-renders
-
+//! We have 12 item in the list and if we check the console when we keep on clicking values are keep on increasing.
+//! Each and every time we click a button we change the count value with useState (cause re-render)
+//! And in the BigList component which we use map() function also re-render.
+//! Solution is memo function (method) comes with React. Do not confuse it with useMemo.
+//! Use React.memo() and wrap the all logic into the paranthesis.
+//! Now when we click the button we are not calling the useEffect each and every time. Because memo is checking (memorizing) what is the value in the props. If the props value did not change, then we are not triggering the re-render. Remember everytime the props or state changes component re-renders. But here we triggering re-render with count and using React.memo what we do is checking did the values of the props changing or it is exactly the same. For this case it is same. So React decide that there is no need to re-render the component.
 const Index = () => {
   const { products } = useFetch(url)
   const [count, setCount] = useState(0)
@@ -22,7 +25,10 @@ const Index = () => {
   )
 }
 
-const BigList = ({ products }) => {
+const BigList = React.memo(({ products }) => {
+  useEffect(() => {
+    console.log('big list called')
+  })
   return (
     <section className='products'>
       {products.map((product) => {
@@ -30,9 +36,12 @@ const BigList = ({ products }) => {
       })}
     </section>
   )
-}
+})
 
 const SingleProduct = ({ fields }) => {
+  useEffect(() => {
+    console.count('single item called')
+  })
   let { name, price } = fields
   price = price / 100
   const image = fields.image[0].url
